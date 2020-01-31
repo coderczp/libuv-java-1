a6097ef4eb480cdfcd59a75649369b9a34d86612
webfolderio <support@webfolder.io>
2020-01-31 22:56:57 +0300

    Build msvc library with MD not MT

--- src/main/build.bat
+++ src/main/build.bat
@@ -1,5 +1,5 @@
 mkdir build 2> NUL
 cd build
-cmake .. -G"Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
+cmake .. -G"Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-dynamic-crt
 cmake --build . --config Release
 cd ..
