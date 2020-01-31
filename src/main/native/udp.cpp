83fedc663d83d87222468141a80ce6ed0cf8a2d3
webfolderio <support@webfolder.io>
2020-01-31 15:50:37 +0300

    Fixed broken build for Linux

--- src/main/native/udp.cpp
+++ src/main/native/udp.cpp
@@ -34,6 +34,10 @@
 #include "udp.h"
 #include "com_oracle_libuv_handles_UDPHandle.h"
 
+#ifndef ULONG
+#define ULONG unsigned long
+#endif
+
 typedef void(*uv_alloc_cb)(uv_handle_t* handle,
 	size_t suggested_size,
 	uv_buf_t* buf);
