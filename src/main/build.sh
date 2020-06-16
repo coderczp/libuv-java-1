c88e5bf0554da4e934c2b0e58affe6e4200c036e
WebFolder OÜ <support@webfolder.io>
2020-06-16 16:39:55 +0300

    fixed build error [build centos]

--- src/main/build.sh
+++ src/main/build.sh
@@ -2,16 +2,18 @@ mkdir -p install
 wget https://github.com/libuv/libuv/archive/v1.38.0.tar.gz
 tar xfz v1.38.0.tar.gz
 cd libuv-1.38.0/
-mv libuv-1.38.0/ libuv/
 mkdir -p build
 cd build
 cmake .. -DCMAKE_C_FLAGS=-fPIC -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
 ninja
+ninja install
+cd ..
 cd ..
 mkdir -p build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
 ninja
+cd ..
 mkdir -p ../resources/META-INF
-cp libuv-java.so ../resources/META-INF/libuv-java.so
+cp build/libuv-java.so ../resources/META-INF/libuv-java.so
 cd ..
