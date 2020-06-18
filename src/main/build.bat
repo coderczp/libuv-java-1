8007a52f5176512913966cea32b67cb6cedc3937
webfolderio <support@webfolder.io>
2020-06-18 06:43:19 +0300

    Added windows build script

--- src/main/build.bat
+++ src/main/build.bat
@@ -1,5 +1,18 @@
+-L https://github.com/libuv/libuv/archive/v1.38.0.tar.gz
+tar xfz v1.38.0.tar.gz
+cd libuv-1.38.0/
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
+ninja
+ninja install
+cd ..
+cd ..
 mkdir build 2> NUL
 cd build
-cmake .. -G"Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-dynamic-crt
-cmake --build . --config Release
+cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
+ninja
+cd ..
+mkdir ..\main\resources\META-INF 2> NUL
+copy build\libuv-java.so ..\main\resources\META-INF\libuv-java.dll
 cd ..
