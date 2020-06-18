9a5fec89aa6a741a68e2eefda4410e5c93c149ea
webfolderio <support@webfolder.io>
2020-06-18 07:18:05 +0300

    [build windows]

--- src/main/build.bat
+++ src/main/build.bat
@@ -3,14 +3,14 @@ tar xfz v1.38.0.tar.gz
 cd libuv-1.38.0/
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
+cmake .. -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
 ninja
 ninja install
 cd ..
 cd ..
 mkdir build 2> NUL
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
+cmake .. -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
 ninja
 cd ..
 mkdir ..\main\resources\META-INF 2> NUL
