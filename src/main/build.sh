694276e9ad0fa9f9e78806584623a2155c3f49b6
WebFolder OÜ <support@webfolder.io>
2020-06-23 18:19:23 +0300

    [build macos]

--- src/main/build.sh
+++ src/main/build.sh
@@ -4,15 +4,15 @@ tar xfz v1.38.0.tar.gz
 cd libuv-1.38.0/
 mkdir -p build
 cd build
-cmake .. -DCMAKE_C_FLAGS=-fPIC -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
-ninja
-ninja install
+cmake .. -DCMAKE_C_FLAGS=-fPIC -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release
+make
+make install
 cd ..
 cd ..
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -GNinja
-ninja
+cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release
+make
 cd ..
 mkdir -p ../main/resources/META-INF
 cp build/libuv-java.so ../main/resources/META-INF/libuv-java.so
