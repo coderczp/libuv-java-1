7274504106a0854e1bf9e507f86e0e6437c9ca1f
WebFolder OÜ <support@webfolder.io>
2020-06-23 16:57:06 +0300

    [build windows]

--- src/main/build.bat
+++ src/main/build.bat
@@ -5,14 +5,14 @@ mkdir -p build
 cd build
 cmake .. -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../../install -G "Visual Studio 15 2017" -A x64
 cmake --config Release --build .
-cmake -- build .
+msbuild uv_a.vcxproj
 cd ..
 cd ..
 mkdir build 2> NUL
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../install -G "Visual Studio 15 2017" -A x64
 cmake --config Release --build .
-cmake -- build .
+msbuild uv_a.vcxproj uv-java.vcxproj
 cd ..
 mkdir ..\main\resources\META-INF 2> NUL
 copy build\libuv-java.so ..\main\resources\META-INF\libuv-java.dll
