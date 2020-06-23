b45fc298407c80adb2ba06d9f5bca1489bf8336c
WebFolder OÜ <support@webfolder.io>
2020-06-23 16:19:09 +0300

    [build windows]

--- src/main/build.bat
+++ src/main/build.bat
@@ -3,14 +3,14 @@ tar xfz v1.38.0.tar.gz
 cd libuv-1.38.0/
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017" -A x64
-cmake --build . --config Release
+cmake .. -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017" -A x64
+cmake --config Release --build .
 cd ..
 cd ..
 mkdir build 2> NUL
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017" -A x64
-cmake --build . --config Release
+cmake --config Release --build .
 cd ..
 mkdir ..\main\resources\META-INF 2> NUL
 copy build\libuv-java.so ..\main\resources\META-INF\libuv-java.dll
