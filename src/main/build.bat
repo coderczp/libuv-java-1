621ee436a12e242a45a288afbcc5702b0b78e18d
WebFolder OÜ <support@webfolder.io>
2020-06-23 15:41:53 +0300

    [build windows]

--- src/main/build.bat
+++ src/main/build.bat
@@ -3,13 +3,13 @@ tar xfz v1.38.0.tar.gz
 cd libuv-1.38.0/
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G"Visual Studio 15 2017 Win64"
+cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -A x64
 cmake --build . --config Release
 cd ..
 cd ..
 mkdir build 2> NUL
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G"Visual Studio 15 2017 Win64"
+cmake .. -DCMAKE_INSTALL_PREFIX=../../install -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -A x64
 cmake --build . --config Release
 cd ..
 mkdir ..\main\resources\META-INF 2> NUL
